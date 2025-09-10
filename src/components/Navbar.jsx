91c9dcde08ade98b7a228c72e51df8b59e641673
Mohamed Ashraf <mohammed.ashraf.m.w@gmail.com>
2025-09-10 07:21:46 +0300

    Update Navbar logo link to LinkedIn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,47 @@ import mylogo from "../assets/images/mylogo.png";
 
 const Navbar = () => {
     return ( <
-        header className = 'flex items-center justify-between px-4 py-2 header' > { /* Clickable Rounded Logo to LinkedIn */ } <
+        header className = "flex items-center justify-between px-4 py-2 header" >
+
+        { /* Clickable Rounded Logo to LinkedIn */ } <
         a href = "https://www.linkedin.com/in/mohamed-ashraf-de/"
         target = "_blank"
         rel = "noopener noreferrer" >
         <
         img src = { mylogo }
-        alt = 'Mohamed Ashraf Logo'
-        className = 'object-cover w-20 h-20 transition-transform duration-200 rounded-full hover:scale-105 hover:shadow-xl' /
+        alt = "Mohamed Ashraf Logo"
+        className = "object-cover w-20 h-20 transition-transform duration-200 rounded-full hover:scale-105 hover:shadow-xl" /
         >
         <
         /a>
 
         { /* Navigation Links */ } <
-        nav className = 'flex text-lg font-medium gap-7' >
+        nav className = "flex text-lg font-medium gap-7" >
         <
-        NavLink to = '/about'
+        NavLink to = "/about"
         className = {
             ({ isActive }) =>
             isActive ? "text-blue-600" : "text-black"
         } >
         About <
-        /NavLink> <
-        NavLink to = '/projects'
+        /NavLink>
+
+        <
+        NavLink to = "/projects"
         className = {
             ({ isActive }) =>
             isActive ? "text-blue-600" : "text-black"
         } >
         Projects <
+        /NavLink>
+
+        <
+        NavLink to = "/contact"
+        className = {
+            ({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+        } >
+        Contact <
         /NavLink> <
         /nav> <
         /header>
